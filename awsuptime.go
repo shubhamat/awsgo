09bffdf35eeadda4939f71db1c1a9fb3fb8ededf
sam <shubham.mankhand@gmail.com>
2017-01-12 15:18:16 -0800

    Loop through all Reservations and Instances

--- awsuptime.go
+++ awsuptime.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-//	"github.com/aws/aws-sdk-go/aws"
+	//	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
-  "time"
+	"os"
+	"time"
 )
 
 func main() {
@@ -13,12 +14,26 @@ func main() {
 		SharedConfigState: session.SharedConfigEnable,
 	}))
 
-	ec2Svc := ec2.New(s)
-  result, err := ec2Svc.DescribeInstances(nil)
-  //re := regexp.MustCompile("LaunchTime*,")
+	esvc := ec2.New(s)
+	result, err := esvc.DescribeInstances(nil)
+	//re := regexp.MustCompile("LaunchTime*,")
 	if err != nil {
-		fmt.Println("Error", err)
+		fmt.Println("Error fetching Instances from AWS", err)
+		os.Exit(1)
 	} else {
-    fmt.Println(time.Since(*result.Reservations[0].Instances[0].LaunchTime))
+		printInstanceUpTime(result)
 	}
+}
+
+func printInstanceUpTime(desc *ec2.DescribeInstancesOutput) {
+	fmt.Printf("PublicIpAddress\t\tUpTime\n")
+	for _, r := range desc.Reservations {
+		for _, i := range r.Instances {
+			if *i.State.Name == "running" {
+				fmt.Printf("%s\t\t%s\n", *i.PublicIpAddress, time.Since(*i.LaunchTime))
+			}
+		}
+
+	}
+
 }
