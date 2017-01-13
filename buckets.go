1c9fb45c6403ab9b597e38b33448209fb2601b26
sam <shubham.mankhand@gmail.com>
2017-01-13 00:23:32 -0800

    Added code to upload file

--- buckets.go
+++ buckets.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3manager"
 	"os"
 )
 
@@ -15,6 +16,7 @@ var list = flag.Bool("list", false, "List Buckets on S3")
 
 var s *session.Session
 var svc *s3.S3
+var bucket string
 
 func main() {
 
@@ -29,6 +31,36 @@ func main() {
 	if *list {
 		printBuckets()
 	}
+
+	if *ufile != "" && *dfile != "" {
+		fmt.Println("Cannot upload and download at the same time")
+		usage()
+	}
+
+	if *ufile == "" && *dfile == "" {
+		fmt.Println("Must specify --uploadfile or --downloadfile")
+		usage()
+	}
+
+	b := flag.Args()
+	if len(b) != 1 {
+		fmt.Println("Must specify bucket name with --uploadfile or --downloadfile")
+		usage()
+	}
+
+	bucket = b[0]
+
+	if *ufile != "" {
+		upload()
+	} else {
+		download()
+	}
+
+}
+
+func usage() {
+	fmt.Println("Usage: bucket [--list] | [ --uploadfile=<filename> | --downloadfile=<filename> ] bucketname")
+	os.Exit(1)
 }
 
 func printBuckets() {
@@ -41,4 +73,33 @@ func printBuckets() {
 	for _, b := range result.Buckets {
 		fmt.Printf("%s\n", *b.Name)
 	}
+	os.Exit(0)
+}
+
+func upload() {
+	file, err := os.Open(*ufile)
+	if err != nil {
+		fmt.Printf("Unable to open file %q\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	uploader := s3manager.NewUploader(s)
+
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(*ufile),
+		Body:   file,
+	})
+	if err != nil {
+		// Print the error and exit.
+		fmt.Printf("Unable to upload %q to %q, %v", *ufile, bucket, err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Successfully uploaded %q to %q\n", *ufile, bucket)
+}
+
+func download() {
+
 }
