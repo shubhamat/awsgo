cb15a44c67c88fa00cd771ca0623ee8c499fa975
sam <shubham.mankhand@gmail.com>
2017-01-12 23:50:06 -0800

    Added --list flag

--- buckets.go
+++ buckets.go
@@ -3,29 +3,42 @@ package main
 import (
 	"fmt"
 	//	"github.com/aws/aws-sdk-go/aws"
+	"flag"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"os"
 )
 
+var ufile = flag.String("uploadfile", "", "File to upload to S3 bucket")
+var dfile = flag.String("downloadfile", "", "File to download from S3 bucket")
+var list = flag.Bool("list", false, "List Buckets on S3")
+
+var s *session.Session
+var svc *s3.S3
+
 func main() {
-	s := session.Must(session.NewSessionWithOptions(session.Options{
+
+	flag.Parse()
+
+	s = session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
 	}))
 
-	svc := s3.New(s)
+	svc = s3.New(s)
+
+	if *list {
+		printBuckets()
+	}
+}
+
+func printBuckets() {
 	result, err := svc.ListBuckets(nil)
 	if err != nil {
 		fmt.Println("Error fetching Instances from AWS", err)
 		os.Exit(1)
-	} else {
-		printBuckets(result)
 	}
-}
-
-func printBuckets(o *s3.ListBucketsOutput) {
 	fmt.Printf("Buckets\n")
-	for _, b := range o.Buckets {
+	for _, b := range result.Buckets {
 		fmt.Printf("%s\n", *b.Name)
 	}
 }
